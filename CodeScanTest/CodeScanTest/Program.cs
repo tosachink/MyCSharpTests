a3bb95aedb647c67d3d8d3f4e93b74773db1c220
skeskar1 <sachin_keskar@intuit.com>
2025-01-21 09:42:12 +0530

    Initial checkin of CodeScanTest

---
+++ CodeScanTest/CodeScanTest/Program.cs
@@ -0,0 +1,19 @@
+﻿// Sample Code for Code Scanning with Code QL
+// Author: Sachin Keskar
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeScanTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
