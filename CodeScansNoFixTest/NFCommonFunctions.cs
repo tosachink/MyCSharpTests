f385c0383f0ce898086da7f3a63d4bf000240d86
skeskar1 <sachin_keskar@intuit.com>
2025-01-22 20:11:24 +0530

    Added Non Fixed Branch for demo

---
+++ CodeScansNoFixTest/NFCommonFunctions.cs
@@ -0,0 +1,27 @@
+﻿// Sample Code for Code Scanning with Code QL
+// Author: Sachin Keskar
+
+
+using System;
+using System.IO;
+
+
+namespace CodeScansNoFixTest
+{
+    internal class NFCommonFunctions
+    {
+        public static void nfprintme(Object obj)
+        {
+            Console.WriteLine(obj);
+        }
+
+        public static void nfcreateFile(String fileName)
+        {
+            if (fileName != null && fileName.Length >= 0)
+            {
+                File.Delete(fileName);
+                File.Create(fileName);
+            }
+        }
+    }
+}
---
+++ CodeScansNoFixTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeScansNoFixTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Dynamic Test call
+            dynamic obj = new DynamicTest();
+            obj.printme(null);
+
+            // Invalid API Test
+            NFCommonFunctions.nfprintme(null);
+
+            // Create File Test
+            NFCommonFunctions.nfcreateFile("");
+
+            // CSS Vulnerability
+            NFCSSVulnerability c = new NFCSSVulnerability();
+        }
+    }
+}
